4ba38de7c04074d2285fc6f1f8fd21fc5b298511
bpaucinac <157474322+bpaucinac@users.noreply.github.com>
2025-03-26 07:52:21 +0100

    Initial Commit

---
+++ HummingbirdTests/HummingbirdTests.swift
@@ -0,0 +1,17 @@
+//
+//  HummingbirdTests.swift
+//  HummingbirdTests
+//
+//  Created by Boris Paucinac on 3/26/25.
+//
+
+import Testing
+@testable import Hummingbird
+
+struct HummingbirdTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
